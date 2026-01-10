0252859e258b2ff42045b112083ed5c2ec2a2813
Ian Heggie (OAF) <ian+oaf@heggie.biz>
2026-01-11 00:19:47 +1100

    Update scraper, add pauses, start from referincing page

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,8 @@
 require 'scraperwiki'
 require 'mechanize'
 
+INITIAL_PAGE_URL = "https://www.yarracity.vic.gov.au/planning-and-building/planning-permits".freeze
+
 url_base = "https://www.yarracity.vic.gov.au/planning-application-search"
 url = url_base + "?suburb=(All)&street=(All)&status=Current&ward=(All)"
 
@@ -41,20 +43,45 @@ end
 agent = Mechanize.new
 agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-page = agent.get url
+start_time = Time.now.to_f
+initial_page = agent.get INITIAL_PAGE_URL
 
-begin
+# Find and click the "View advertised applications" link
+view_apps_link = initial_page.links.find { |link| link.text.include?("View advertised applications") }
+duration = (Time.now.to_f - start_time).round(3) + 0.5
+puts "Pausing #{duration}s"
+sleep(duration)
+
+start_time = Time.now.to_f
+if view_apps_link
+  page = view_apps_link.click
+  # Now 'page' should be the page with the advertised applications
+else
+  raise "Could not find 'View advertised applications' link"
+end
+duration = (Time.now.to_f - start_time).round(3) + 0.5
+
+page_no = 0
+while page_no < 100
   get_page_data(page, url_base)
 
-  # Click on the link to the next page
   links = page.search('div.pagination-container').search('a')
-  link = links.find{|a| a.inner_text == 'Next'}
+  next_page_link = links.find{|a| a.inner_text == 'Next'}
 
-  if link
-    puts url_base + link["href"]
-    page = agent.get (url_base + link["href"])
+  unless next_page_link
+    puts "Finished - no Next page"
+    break
   end
-  # end
 
-end while link
+  puts "Pausing #{duration}s"
+  sleep(duration)
+
+  page_no += 1
+  puts
+  puts "Getting page: #{page_no}: #{url}"
+  start_time = Time.now.to_f
+  url = url_base + next_page_link["href"]
+  page = agent.get url
+  duration = (Time.now.to_f - start_time).round(3) + 0.5
+end
 
